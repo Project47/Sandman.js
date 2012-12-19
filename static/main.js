507c34f8ddb293d86bb33c2de0dc8fcfc2958e05
Parth <parth@ubuntu.ubuntu-domain>
2012-12-19 15:46:38 +0530

    Rounding, Comparison with threshold 24

--- static/main.js
+++ static/main.js
@@ -17,33 +17,51 @@ function doFirst()
 	twiddle=new Array();
 	twiddle=[[1,0],[0.924,-0.387],[0.707,-0.707],[0.387,-0.924],[0,-1],[-0.387,0.924],[-0.707,-0.707],[-0.927,0.387]];
     	samplePt=0;
-     	inputPtr=0;
-    	mouseFlag=0;
-	x=document.getElementById("canv");
-    	context=x.getContext('2d');   
-}
+    	maxX=-9999;
+    	maxY=-9999;
+    	minY=9999;
+    	minX=9999;
+    scaleFactor=0;    
+	 inputPtr=0;
+	 mouseFlag=0;
+    gesture=new Array();
+    gestPtr=0;
+	 x=document.getElementById("canv");
+	 context=x.getContext('2d');   
+ }
 
 
-function touchStart(e)
-{
-   // if(cnt==1) return;
-// To prevent  default browser scrolling
-    mouseFlag=1;
-    e.preventDefault();	    
-    count=0;
-}
+ function touchStart(e)
+ {
+    // if(cnt==1) return;
+ // To prevent  default browser scrolling
+     mouseFlag=1;
+    	maxX=-9999;
+    	minX=9999;
+	maxY=-9999;
+    	minY=9999;
+    
+     e.preventDefault();	    
+     count=0;
+ }
 
-function mouseMove(e)
-{
-    if(mouseFlag==1)
-    {
+ function mouseMove(e)
+ {
+     if(mouseFlag==1)
+     {
+	 if(e.clientX<minX){ minX=e.clientX;}
+	 if(e.clientX>maxX) {maxX=e.clientX;}
+
+	 if(e.clientY<minY){ minY=e.clientY;}
+	 if(e.clientY>maxY) {maxY=e.clientY;}
+
     inputX[inputPtr]=e.clientX;
     inputY[inputPtr]=e.clientY;
     inputPtr++;
     count=count+1;
 	context.beginPath();
     context.arc(e.clientX, e.clientY, 1, 0, Math.PI, true);
-	context.strokeStyle='red';
+	context.strokeStyle='cyan';
 	context.stroke();
     }
 
@@ -54,6 +72,11 @@ function touchMoving(e)
     
 // Taking the input points
     mouseFlag=0;
+    if(e.touches[0].pageX<minX){ minX=e.touches[0].pageX;}
+    if(e.touches[0].pageX>maxX) {maxX=e.touches[0].pageX;}
+    if(e.touches[0].pageY<minY){ minY=e.touches[0].pageY;}
+    if(e.touches[0].pageY>maxY) {maxY=e.touches[0].pageY;}
+   
     inputX[inputPtr]=e.touches[0].pageX;
     inputY[inputPtr]=e.touches[0].pageY;
     inputPtr++;
@@ -80,12 +103,15 @@ In this function we create array of fixed number of  evenly spaced pixels from t
    // cnt=1;
 
 	i=1;
+   /// alert("asdfghjhgfds");
 	var leng=path_length();
 	interval=(leng/17);
 	tempDist=0;
     mouseFlag=0;
     	add=0;
 	samplePt=0;
+    scaleFactor=Math.sqrt((maxX-minX)*(maxX-minX)+(maxY-minY)*(maxY-minY))/100;
+    //alert("scaleFactor:"+scaleFactor +"min:" + minX + "max: " + maxX);
     while(i<inputPtr)
     {
 	add=(inputX[i]-inputX[i-1])*(inputX[i]-inputX[i-1])+(inputY[i]-inputY[i-1])*(inputY[i]-inputY[i-1]);
@@ -129,12 +155,116 @@ document.getElementById("temp").innerHTML=document.getElementById("temp").innerH
 //output=new Array();
 
     document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "<br />";
-output=fft(sampled2d,16);
 
+output=fft(sampled2d,16);
+    rounded=output;
+    var temp=0;
+    var k=0;
 for(i=0;i<16;i++)
 {
-    document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "o1: "+Math.round(output[i][0])+"o2: " + Math.round(output[i][1]) + " | ";
+    temp=Math.round(output[i][0]/scaleFactor);
+    if(temp<-10000) 
+    {
+	rounded[i][0]=-10000;
+    }
+    else if(temp>10000)
+    {
+	rounded[i][0]=10000;
+    }
+    else if(temp<-1000 || temp>1000 )
+    {
+	if(temp<-1000)
+	    temp=temp-50;
+	else
+	    temp=temp+50;
+	k=Math.round(temp/100);
+	rounded[i][0]=k*100;
+    }
+    else if(temp<-100 || temp>100)
+    {
+	if(temp<-100)
+	temp=temp-5;
+	else 
+	temp=temp+5;
+	k=Math.round(temp/10);
+	rounded[i][0]=k*10;
+    }
+    else if(temp<-10 || temp>10)
+    {
+	if(temp<-10)
+	temp=temp-2.5;
+	else
+	    	temp=temp+2.5;
+	k=Math.round(temp/5);
+	rounded[i][0]=k*5;
+    }
+    else
+    {
+	rounded[i][0]=Math.round(temp);
+    }
+
+  temp=Math.round(output[i][1]/scaleFactor);
+    if(temp<-10000) 
+    {
+	rounded[i][1]=-10000;
+    }
+    else if(temp>10000)
+    {
+	rounded[i][1]=10000;
+    }
+    else if(temp<-1000 || temp>1000 )
+    {
+	if(temp<-1000)
+	    temp=temp-50;
+	else
+	    temp=temp+50;
+	k=Math.round(temp/100);
+	rounded[i][1]=k*100;
+    }
+    else if(temp<-100 || temp>100)
+    {
+	if(temp<-100)
+	temp=temp-5;
+	else 
+	temp=temp+5;
+	k=Math.round(temp/10);
+	rounded[i][1]=k*10;
+    }
+    else if(temp<-10 || temp>10)
+    {
+	if(temp<-10)
+	temp=temp-2.5;
+	else
+	    	temp=temp+2.5;
+	k=Math.round(temp/5);
+	rounded[i][1]=k*5;
+    }
+    else
+    {
+	rounded[i][1]=Math.round(temp);
+    }
+
+    document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "| x: "+ rounded[i][0]+" y: " + rounded[i][1] + " | ";
+
 }
+
+    gesture[gestPtr]=rounded;
+    gestPtr++;
+    var resCnt=0;
+    if(gestPtr==2)
+    {
+	var gest1=gesture[0];
+	var gest2=gesture[1];
+	for(i=0;i<16;i++)
+	{
+	    if(gest1[i][0]==gest2[i][0]) resCnt++;
+	    if(gest1[i][0]==gest2[i][0]) resCnt++;
+	    if(resCnt>=24) {alert("Yesss"); break;}
+	}
+	gestPtr=0;
+    if(resCnt<15) alert("rescnt:" + resCnt);
+
+    }
 
     document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "<br /><br />";
 chainInputPtr=0;
