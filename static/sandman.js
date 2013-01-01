69d12bf88cb7c1708f6675ed3870a07dab418fa7
Parth <parth@ubuntu.ubuntu-domain>
2013-01-01 11:05:31 +0530

    Taking input for multi-stroke gestures

--- static/sandman.js
+++ static/sandman.js
@@ -3,67 +3,78 @@
 function doFirst (domElement) {
 
   var ck=1;
-var evt=0;
+  var evt=0;
 
+  var input2d=new Array ();
+  var     inputPtr=0;
+  var  mouseFlag=0;
+var okToSample=1;
+var intervalSet=0;
+var strokeInterval=null;
+  var minMax= new Array () ;
+  minMax=[-9999,-9999,9999,9999];      //[ maxX, maxY, minX, minY ]
 
-//inputX = new Array();
-  //  inputY = new Array();
-
-var input2d=new Array ();
-var     inputPtr=0;
-var  mouseFlag=0;
-var minMax= new Array () ;
-minMax=[-9999,-9999,9999,9999];      //[ maxX, maxY, minX, minY ]
-
- document.getElementById(domElement).addEventListener("touchmove",touchMoving,false);
+  document.getElementById(domElement).addEventListener("touchmove",touchMoving,false);
   document.getElementById(domElement).addEventListener("touchstart",touchStart ,false);
-    document.getElementById(domElement).addEventListener("touchend",sample,false);
-  document.getElementById(domElement).addEventListener("mousedown",function (e) {inputPtr=0; mouseFlag=1;touchStart (e);
-                                                                                  }    ,false
+  document.getElementById(domElement).addEventListener("touchend",sample,false);
+  document.getElementById(domElement).addEventListener("mousedown",function (e) {okToSample=0;  minMax=[-9999,-9999,9999,9999];   mouseFlag=1;touchStart (e);
+                                                                                }    ,false
                                                       );
 
 
-var event=null;
-    document.getElementById(domElement).addEventListener("mousemove",function (e) {
+  var event=null;
+  document.getElementById(domElement).addEventListener("mousemove",function (e) {
 
-if (evt===0) {
-evt=1;
-//alert ("inevent");
-event=  document.getElementById(domElement).addEventListener("mouseup",function (e)  {   mouseFlag=0;sample (inputPtr,input2d,minMax); //alert (input2d [inputPtr]);
-                                                                                } ,false);
-//alert ("outeventE");
+    if (evt===0) {
+      evt=1;
+      //alert ("inevent");
+      event=  document.getElementById(domElement).addEventListener("mouseup",function (e)  {
+ mouseFlag=0;
+
+okToSample=1;
+
+var breakPoint=inputPtr-1;
+if (intervalSet==0)
+        {
+intervalSet=1;
+strokeInterval=setInterval ( function () {
+
+if (okToSample==1) {
+
+  clearInterval (strokeInterval);
+sample (inputPtr,input2d,minMax,breakPoint);
+intervalSet=0;
+inputPtr=0;
 }
-if (mouseFlag==1) { input2d=mouseMove (e,inputPtr,input2d,minMax);   inputPtr++;
-                                                                                }  },false);
+
+},1000); //alert (input2d [inputPtr]);
+        }
+
+                                                                                   } ,false);
+      //alert ("outeventE");
+    }
+    if (mouseFlag==1) { input2d=mouseMove (e,inputPtr,input2d,minMax);   inputPtr++;
+                      }  },false);
 
 
 
-    maxX=-9999;
-    maxY=-9999;
-    minY=9999;
-    minX=9999;
-
-
-    gesture=new Array();
+  gesture=new Array();
   gestPtr=0;
-    x=document.getElementById(domElement);
-    context=x.getContext('2d');
+  x=document.getElementById(domElement);
+  context=x.getContext('2d');
 
   context.beginPath();
-    context.arc(200,200, 1, 0, Math.PI, true);
-    context.strokeStyle='black';
-    context.stroke();}
+  context.arc(200,200, 1, 0, Math.PI, true);
+  context.strokeStyle='black';
+  context.stroke();}
 
 function touchStart (e) {
   // if(cnt
   // ==1) return;
   // To prevent  default browser scrolling
 
-//  mouseFlag=1;
-  maxX=-9999;
-  minX=9999;
-  maxY=-9999;
-  minnY=9999;
+  //  mouseFlag=1;
+
   e.preventDefault();
 
 }
@@ -71,30 +82,30 @@ function touchStart (e) {
 function mouseMove(e,ptr,array,minMax)
 {
 
-    if(e.clientX<minMax [2]){ minMax [2]=e.clientX;}
-    if(e.clientX>minMax [0]) {minMax [0]=e.clientX;}
+  if(e.clientX<minMax [2]){ minMax [2]=e.clientX;}
+  if(e.clientX>minMax [0]) {minMax [0]=e.clientX;}
 
-    if(e.clientY<minMax [3]){ minMax [3]=e.clientY;}
-    if(e.clientY>minMax [1]) {minMax [1]=e.clientY;}
+  if(e.clientY<minMax [3]){ minMax [3]=e.clientY;}
+  if(e.clientY>minMax [1]) {minMax [1]=e.clientY;}
 
-    array[ptr]=[e.clientX,e.clientY];
+  array[ptr]=[e.clientX,e.clientY];
 
-    context.beginPath();
-    context.arc(e.clientX, e.clientY, 1, 0, Math.PI, true);
-    context.strokeStyle='cyan';
-    context.stroke();
+  context.beginPath();
+  context.arc(e.clientX, e.clientY, 1, 0, Math.PI, true);
+  context.strokeStyle='cyan';
+  context.stroke();
 
 
-return array;
+  return array;
 
 }
 
 function touchMoving(e)
 {
   // Taking the input points
-/*
+  //
   mouseFlag=0;
-  if(e.touches[0].pageX<minX){ minX=e.touches[0].pageX;}
+  if(e.touches[0].pageX<minX){ minX=e.touches[0].pageX;}       //[ maxX, maxY, minX, minY ]
   if(e.touches[0].pageX>maxX) {maxX=e.touches[0].pageX;}
   if(e.touches[0].pageY<minY){ minY=e.touches[0].pageY;}
   if(e.touches[0].pageY>maxY) {maxY=e.touches[0].pageY;}
@@ -102,11 +113,11 @@ function touchMoving(e)
   inputY[inputPtr]=e.touches[0].pageY;
   inputPtr++;
 
-  context.beginPath();
+  //[ maxX, maxY, minX, minY ]  context.beginPath();
   context.arc(e.clientX, e.clientY, 1, 0, Math.PI, true);
   context.strokeStyle='cyan';
   context.stroke();
-*/
+
 }
 
 /*
@@ -117,34 +128,39 @@ function touchMoving(e)
 
   In this function we create array of fixed number of  evenly spaced pixels from the input array array using interpolation.
 */
-function sample(ptr,array,minMax){
+function sample(ptr,array,minMax,breakPoint){
 
   //INTERPOLATING
-//alert (ptr);
+  //alert (ptr);
 
-//alert ("s"+array [0 ] [1]
-//      );
+  //alert ("s"+array [0 ] [1]
+  //      );
   i=1;
-alert (minMax [1]);
-  var leng=path_length(ptr,array)
-var sampledX = new Array();
-var    sampledY = new Array();
-var   samplePt=0;
-  interval=(leng/15);
-  tempDist=0;
- // mouseFlag=0;
-  add=0;
+  //alert (minMax [1]);
+  var leng=path_length(ptr,array,breakPoint);
+  var sampledX = new Array();
+  var    sampledY = new Array();//[ maxX, maxY, minX, minY ]
+  var   samplePt=0;
+  var interval=(leng/15);
+  var tempDist=0;
+  // mouseFlag=0;
+  var add=0;
 
-  samplePt=0;
-var scaleFactor=Math.sqrt((maxX-minX)*(maxX-minX)+(maxY-minY)*(maxY-minY))/100;
+
+  var scaleFactor=Math.sqrt((minMax [0]-minMax [2])*(minMax [0]-minMax [2])+(minMax [1]-minMax [3])*(minMax [1]-minMax [3]))/100;
   sampledX[0]=array[0][0];
   sampledY[0]=array[0][1];
- var samplePt=1;
+  var samplePt=1;
 
   while(i<ptr)
   {
     add=(array[i] [0]-array[i-1] [0])*(array[i] [0]-array[i-1] [0])+(array[i] [1]-array[i-1] [1])*(array[i] [1]-array[i-1] [1]);
     interPixelDist=Math.sqrt(add);
+if (i===breakPoint)
+{
+i++;
+
+}
     if((tempDist+interPixelDist)>=interval)
     {
       sampledX[samplePt]=array[i-1] [0]+((interval-tempDist)/interPixelDist)*(array[i] [0]-array[i-1] [0]);
@@ -182,7 +198,7 @@ var scaleFactor=Math.sqrt((maxX-minX)*(maxX-minX)+(maxY-minY)*(maxY-minY))/100;
     context.strokeStyle='black';
     context.stroke();
   }
-//  document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "<br />";
+  //  document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "<br />";
 
 
   /*  var minnX=9999;
@@ -224,9 +240,9 @@ var scaleFactor=Math.sqrt((maxX-minX)*(maxX-minX)+(maxY-minY)*(maxY-minY))/100;
     }
   }
 
-  output=fft(sampled2d,16);
-  rounded=output;
-//alert (output [0] [0] + "   "+output [0] [1]);
+  var output=fft(sampled2d,16);
+  var rounded=output;
+  //alert (output [0] [0] + "   "+output [0] [1]);
   var temp=0;
   var k=0;
 
@@ -242,7 +258,7 @@ var scaleFactor=Math.sqrt((maxX-minX)*(maxX-minX)+(maxY-minY)*(maxY-minY))/100;
     }
     else if(temp>10000)
 
-  {
+    {
       rounded[i][0]=10000;
     }
     else if(temp<-1000 || temp>1000 )
@@ -317,11 +333,11 @@ var scaleFactor=Math.sqrt((maxX-minX)*(maxX-minX)+(maxY-minY)*(maxY-minY))/100;
     {
       rounded[i][1]=Math.round(temp);
     }
-    //document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "(x: "+ sampled2d[i][0]+" y: " + sampled2d[i][1] + " ) ";
-    //document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "| x: "+ rounded[i][0]+" y: " + rounded[i][1] + " | ";
+    // document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "(x: "+ sampled2d[i][0]+" y: " + sampled2d[i][1] + " ) ";
+    document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "| x: "+ rounded[i][0]+" y: " + rounded[i][1] + " | ";
   }
 
-  alert (ptr + "   " + gestPtr );
+  // alert (ptr + "   " + gestPtr );
   gesture[gestPtr]=rounded;
   gestPtr++;
   var resCnt=0;
@@ -343,9 +359,9 @@ var scaleFactor=Math.sqrt((maxX-minX)*(maxX-minX)+(maxY-minY)*(maxY-minY))/100;
   document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "<br /><br />";
   chainInputPtr=0;
   samplePt=0;
-ptr=0;
-minMax=[-9999,-9999,9999,9999];      //[ maxX, maxY, minX, minY ]
- // inputPtr=0;
+  ptr=0;
+  minMax=[-9999,-9999,9999,9999];      //[ maxX, maxY, minX, minY ]
+  // inputPtr=0;
 }
 
 
@@ -408,34 +424,37 @@ function createChainCode () {
   //fft();
 }
 
-function path_length(ptr, array)
+function path_length(ptr, array,breakPoint)
 {
   var y=1;
   var len=0;
   var temp=0;
 
-
-
- while(y<ptr)
+  while(y<ptr)
   {
+if (y-1===breakPoint)
+{
+y++;
+}
     temp=(array[y] [0]-array[y-1] [0])*(array[y] [0]-array[y-1] [0])+(array[y] [1]-array[y-1] [1])*(array[y] [1]-array[y-1] [1]);
     len = len + Math.sqrt(temp);
     y=y+1;
 
   }
-/*********/
+  /*********/
   return len;
 }
 
 function fft(sampled2d,n)
 {
+  var twiddle=new Array();
+  twiddle=[[1,0],[0.924,-0.387],[0.707,-0.707],[0.387,-0.924],[0,-1],[-0.387,0.924],[-0.707,-0.707],[-0.927,0.387]];
+
   var Aeven=new Array();
   var Aodd=new Array();
   var Veven=new Array();
   var Vodd=new Array();
   var V=new Array();
-    twiddle=new Array();
- twiddle=[[1,0],[0.924,-0.387],[0.707,-0.707],[0.387,-0.924],[0,-1],[-0.387,0.924],[-0.707,-0.707],[-0.927,0.387]];
   if(n==1)
     return sampled2d;
   for(i=0;i<=n-2;i=i+2)
