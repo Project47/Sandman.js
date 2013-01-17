6ac91659806fc4875ba6909e033147811166b297
Parth <parth@ubuntu.ubuntu-domain>
2013-01-17 15:06:36 +0530

    corrected vertical line parameter

---
+++ static/.#sandman.js
@@ -0,0 +1,1 @@
+parth@ubuntu.ubuntu-domain.2709:1358407585
--- static/sandman.js
+++ static/sandman.js
@@ -5,10 +5,23 @@ var Sandman = {
   context:null,
   gesture:null,
   gestPtr:null,
+gestureArray:[
 
+  [["P"],[300,-320],[180,140],[180,-40],[120,-30],[90,-50],[70,-40],[60,-40],[60,-50],[50,-50],[30,-50],[30,-50],[30,-50],[20,-60],[20,-50],[0,-50],[7,-60],[3,-50],[-20,-50],[-20,-50],[-20,-50],[-30,-60],[-40,-50],[-40,-50],[-50,-50],[-70,-60],[-80,-40],[-90,-50],[-140,-70],[-200,-20],[-340,-50]],
+
+  [["S"," "],[500,-360],[360,160],[140,80],[160,-2],[100,-20],[60,8],[60,20],[70,20],[80,4],[80,-30],[70,-40],[40,-50],[20,-50],[10,-40],[20,-40],[20,-50],[7,-50],[-5,-60],[-7,-40],[-5,-40],[6,-40],[30,-70],[2,-100],[-30,-100],[-50,-90],[-80,-100],[-80,-90],[-80,-90],[-70,-100],[-240,-320]],
+
+[["O"],[520,-500],[240,30],[140,-30],[120,-30],[90,-30],[70,-30],[60,-30],[50,-40],[30,-50],[30,-60],[40,-60],[40,-60],[40,-50],[40,-40],[30,-50],[20,-20],[10,-60],[9,-50],[9,-40],[-3,-30],[-20,-30],[-30,-30],[-40,-40],[-40,-50],[-50,-60],[-60,-60],[-70,-70],[-90,-70],[-140,-50],[-180,-140]]
+
+],
+
+s21101 : [0],
+s20001 : [2],  //O
+s10001 : [1],
+set : null,
 
   touchStart: function  (e) {
-     e.preventDefault();
+    e.preventDefault();
   },
 
 
@@ -24,10 +37,10 @@ var Sandman = {
     var index= [];
     var partitionEntered= [-99,0,0,0,0];           //Partiotion of the block entered
     var xanchorPoint=0;              //Anchored x coordinate value
-    var yanchorpoint=0;            //Anchored x coordinate value
+    var yanchorPoint=0;            //Anchored x coordinate value
     var iterator=0;             //Iterator for the main while loop
-    var horizontalThreshold=2;            //Threshold
-    var verticalThreshold=2;             //Threshold
+    var horizontalThreshold=10;            //Threshold
+    var verticalThreshold=10;             //Threshold
     var verticalLine=0;//
     var horizontalLine=0;//
     var xpointCounter=0;//
@@ -39,8 +52,7 @@ var Sandman = {
     var priority= [];
     var portion=[0,0,0,0,0,0];
     var dist=0;
-
-    while(iterator<pointCount-1){
+    while(iterator<pointCount-1) {
       //Find vertical line
 
 
@@ -70,7 +82,7 @@ var Sandman = {
       }
       //Find horizontal line
 
-     if(Math.abs(points[iterator][1]-points[iterator+1][1])<horizontalThreshold){
+      if(Math.abs(points[yanchorPoint][1]-points[iterator+1][1])<horizontalThreshold){
         ypointCounter++;
       }
       else if(horizontalLine<ypointCounter){
@@ -90,8 +102,8 @@ var Sandman = {
       //Find blocks in which gesture exists
 
       //
-      if(points[iterator][0]<avgX){
-        if(points[iterator][1]<avgY){
+      if(points[iterator][0]<( avgX+avgX/10)){
+        if(points[iterator][1]<( avgY+avgY/10)){
           partitionEntered[1]++;
         }
         else{
@@ -115,33 +127,33 @@ var Sandman = {
           1: iterator
           2: crossoverIterator
 
-        var X11=points [iterator] [0];
-        var Y11=points [iterator] [1];
-        var X12=points [iterator+1] [0];
-        var Y12=points [iterator+1] [1];
+          var X11=points [iterator] [0];
+          var Y11=points [iterator] [1];
+          var X12=points [iterator+1] [0];
+          var Y12=points [iterator+1] [1];
 
-        var X21=points [crossoverIterator] [0];
-        var Y21=points [crossoverIterator] [1];
-        var X22=points [crossoverIterator+1] [0];
-        var Y22=points [crossoverIterator+1] [1];
+          var X21=points [crossoverIterator] [0];
+          var Y21=points [crossoverIterator] [1];
+          var X22=points [crossoverIterator+1] [0];
+          var Y22=points [crossoverIterator+1] [1];
 
-        var A1=Y11-Y12;
-        var B1=X12-X11;
-        var C1=(-1)*B1*Y11+(-1)*A1*X11;
+          var A1=Y11-Y12;
+          var B1=X12-X11;
+          var C1=(-1)*B1*Y11+(-1)*A1*X11;
 
-        var A2=Y21-Y22;
-        var B2=X22-X21;
-        var C2=(-1)*B2*Y21+(-1)*A2*X21;
+          var A2=Y21-Y22;
+          var B2=X22-X21;
+          var C2=(-1)*B2*Y21+(-1)*A2*X21;
 
-        var del=A1*B2-A2*B1;
-        if (del!==0) {
+          var del=A1*B2-A2*B1;
+          if (del!==0) {
           var x=(-1)*(B2*C1 - B1*C2)/del;
           var y=(-1)*(A1*C2 - A2*C1)/del;
-        }
+          }
 
-*/
+        */
 
-    var X11=points [iterator] [0];
+        var X11=points [iterator] [0];
         var Y11=points [iterator] [1];
         var X12=points [iterator+1] [0];
         var Y12=points [iterator+1] [1];
@@ -184,7 +196,7 @@ var Sandman = {
 
       iterator++;
 
-     }
+    }
 
 
     portion [0]=partitionEntered [1] +partitionEntered [2];   //Top half
@@ -192,32 +204,32 @@ var Sandman = {
     portion [2]=partitionEntered [1] +partitionEntered [3];  //Left
     portion [3]=partitionEntered [4] +partitionEntered [2];  //Right
 
-var strokes=1;
-if (breakPoint>0) {
-strokes=2;
-}
+    var strokes=1;
+    if (breakPoint>0) {
+      strokes=2;
+    }
 
-    console.log ("Vert:"+verticalLine+"  Horiz:"+horizontalLine+" Cross:"+crossoverCount+" partition: "+portion.indexOf (Math.max (portion [0],portion [1],portion [2],portion [3]))+"  strokes:"+strokes);
+    alert (" partition: "+portion.indexOf (Math.max (portion [0],portion [1],portion [2],portion [3]))+" Cross:"+crossoverCount+"Vert:"+verticalLine+"  Horiz:"+horizontalLine+"  strokes:"+strokes);
 
 
-/*
-priority[0] = partition
-priority[1] = crossover
-priority[2] = vertical
-priority[3] = horizontal
-priority[4] = strokes (1 or 2)
+    /*
+      priority[0] = partition
+      priority[1] = crossover
+      priority[2] = vertical
+      priority[3] = horizontal
+      priority[4] = strokes (1 or 2)
 
-*/
+    */
 
     priority [0]=portion.indexOf (Math.max (portion [0],portion [1],portion [2],portion [3]));   //partition
     priority [1]=crossoverCount;
-    if (verticalLine>3)
+    if (verticalLine>10)
       priority [2]=1;
     else priority [2]=0;
-    if (horizontalLine>3)
+    if (horizontalLine>10)
       priority [3]=1;
     else priority [3]=0;
-priority [4] = strokes;
+    priority [4] = strokes;
     return priority;
   },
 
@@ -254,7 +266,7 @@ priority [4] = strokes;
 
     array [ptr]=[e.touches [0].pageX,e.touches [0].pageY];
 
-     Sandman.context.beginPath();
+    Sandman.context.beginPath();
     Sandman.context.arc(e.clientX, e.clientY, 1, 0, Math.PI, true);
     Sandman.context.strokeStyle='cyan';
     Sandman.context.stroke();
@@ -276,12 +288,12 @@ priority [4] = strokes;
     {
 
 
-    //
-    if (y-1===breakPoint)
-        {
+      //
+      if (y-1===breakPoint)
+      {
         y++;
-        }
-        /**/
+      }
+      /**/
       temp=(array[y] [0]-array[y-1] [0])*(array[y] [0]-array[y-1] [0])+(array[y] [1]-array[y-1] [1])*(array[y] [1]-array[y-1] [1]);
       len = len + Math.sqrt(temp);
       y=y+1;
@@ -318,14 +330,14 @@ priority [4] = strokes;
     for(i=0;i<n/2;i++)
     {
 
-    //Calculating the real and imaginary parts of the transform
+      //Calculating the real and imaginary parts of the transform
 
-V[i]=[(Veven[i][0]+Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0]-Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]),(Veven[i][1]+Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]+Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0])];
+      V[i]=[(Veven[i][0]+Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0]-Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]),(Veven[i][1]+Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]+Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0])];
 
 
-     V[n/2+i]=[(Veven[i][0]-Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0]+Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]),(Veven[i][1]-Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]-Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0])];
+      V[n/2+i]=[(Veven[i][0]-Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0]+Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]),(Veven[i][1]-Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][1]-Math.sin( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0])];
 
-      }
+    }
 
     return V;
   },
@@ -346,9 +358,9 @@ V[i]=[(Veven[i][0]+Math.cos( (2 * Math.PI * (i*32/n)) / 32 )*Vodd[i][0]-Math.sin
   sample: function (ptr,array,minMax,breakPoint){
 
     //INTERPOLATING
-alert (breakPoint + "    "+ ptr);
+//    alert (breakPoint + "    "+ ptr);
 
-   if (breakPoint===( ptr-1)) {
+    if (breakPoint===( ptr-1)) {
       breakPoint=-1;
     }
     var i=1;
@@ -378,10 +390,10 @@ alert (breakPoint + "    "+ ptr);
       interPixelDist=Math.sqrt(add);
 
       //
-        if (i===breakPoint) {
+      if (i===breakPoint) {
         i++;
-        }
-        /**/
+      }
+      /**/
 
       if((tempDist+interPixelDist)>=interval) {
 
@@ -537,28 +549,49 @@ alert (breakPoint + "    "+ ptr);
       }
 
     }
+for (j=0;j<31;j++)
+{
+  document.getElementById ("temp").innerHTML=document.getElementById ("temp").innerHTML+ "["+rounded [j] [0]+","+rounded [j] [1]+"],";
+}
 
+
+    Sandman.set = "s"+parameterArray [0]+parameterArray [1]+parameterArray [2]+parameterArray [3]+parameterArray [4];
     Sandman.gesture[Sandman.gestPtr]=rounded;
     Sandman.gestPtr++;
     var resCnt=0;
-    if(Sandman.gestPtr===2)
+
+console.log ("set  "+Sandman.set);
+//Sandman.set="s10001";
+if (typeof (window .Sandman[Sandman.set]) === "undefined") {
+console.log ("nope");
+return;
+}
+//Sandman.set  = window.Sandman["set"];
+
+
+
+
+    var gest1=Sandman.gestureArray [ window .Sandman[Sandman.set] [0]];
+
+    var gest2=rounded;
+    for(i=1;i<31;i++)
     {
-      var gest1=Sandman.gesture[0];
-      var gest2=Sandman.gesture[1];
-      for(i=0;i<32;i++)
-      {
-        if(Math.abs(gest1[i][0])===Math.abs(gest2[i][0])) resCnt++;
-          if(Math.abs(gest1[i][1])===Math.abs(gest2[i][1])) resCnt++;
 
+      if(Math.abs(gest1[i][0])===Math.abs(gest2[i][0])) resCnt++;
+      if(Math.abs(gest1[i][1])===Math.abs(gest2[i][1])) resCnt++;
 
-          if(resCnt>=32) {alert("Gesture Matched"); break;}/**/
-
-      }
-
-      Sandman.gestPtr=0;
-      if(resCnt<32) alert("Sandman.Gestures do not match. No. of descriptors matched:" + resCnt);
+      if(resCnt>=10) {
+//alert("Gesture Matched"); break;
+alert ("You have drawn: "+gest1 [0]);
+break;
+}/**/
 
     }
+
+    Sandman.gestPtr=0;
+    if(resCnt<64) alert("Sandman.Gestures do not match. No. of descriptors matched:" + resCnt);
+
+
 
     document.getElementById("temp").innerHTML=document.getElementById("temp").innerHTML + "<br /><br />";
     chainInputPtr=0;
@@ -566,8 +599,9 @@ alert (breakPoint + "    "+ ptr);
     ptr=0;
     minMax=[-9999,-9999,9999,9999];      //[ maxX, maxY, minX, minY ]
 
-  },
+    }
 
+,
   doFirst: function  (domElement) {
 
 
@@ -595,7 +629,7 @@ alert (breakPoint + "    "+ ptr);
       if (eventCalled===0) {
 
         eventCalled=1;
-         event = document.getElementById(domElement).addEventListener("touchend",function (e)  {
+        event = document.getElementById(domElement).addEventListener("touchend",function (e)  {
           mouseFlag=0;
 
           okToSample=1;
